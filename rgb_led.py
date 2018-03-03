9016b027d66ac0432484072cb6ef9b6d6d10b9e6
Alexis Brunet <alexis.brunet@youi.tv>
2018-03-03 22:13:33 +0000

    Added RGB LED experiments

---
+++ rgb_led.py
@@ -0,0 +1,31 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+
+GPIO.setup(21, GPIO.OUT)
+GPIO.setup(20, GPIO.OUT)
+GPIO.setup(16, GPIO.OUT)
+
+def rgb(red, green, blue):
+    GPIO.output(21, GPIO.LOW if not red else GPIO.HIGH)
+    GPIO.output(20, GPIO.LOW if not green else GPIO.HIGH)
+    GPIO.output(16, GPIO.LOW if not blue else GPIO.HIGH)
+
+rgb(False, False, True)
+time.sleep(1)
+rgb(False, True, True)
+time.sleep(1)
+rgb(False, True, False)
+time.sleep(1)
+rgb(True, True, False)
+time.sleep(1)
+rgb(True, False, False)
+time.sleep(1)
+rgb(True, False, True)
+time.sleep(1)
+rgb(True, True, True)
+time.sleep(1)
+rgb(False, False, False)
+
+GPIO.cleanup()
